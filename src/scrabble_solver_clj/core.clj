d25331583ac6a3d18951922f4f5b339f22cf604b
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2019-10-16 21:29:59 +0200

    Fixed find-positions

--- src/scrabble_solver_clj/core.clj
+++ src/scrabble_solver_clj/core.clj
@@ -1,7 +1,8 @@
 (ns scrabble-solver-clj.core)
 
 (def test-board '([0 0 :right "alphabet"]
-                  [0 2 :bottom "piano"]))
+                  [2 0 :bottom "piano"]
+                  [2 2 :right "annee"]))
 
 (defn opposite-direction
   [direction]
@@ -36,29 +37,53 @@
   (frequencies (map (comp keyword str) word)))
 
 (defn find-positions
-  [board [x y direction]]
-  (let [dir (if-right direction x y)
-        opp (if-right direction y x)]
-    (for [start (range 0 12)
-          width (range 2 12)
-          :let [end (+ start width)]
-          :when (< end 12)
-          ;; Does not have a taken tile in left/up direction
-          :when (or (not (> start 0))
-                    (not (tile-taken? board
-                                      (if-right direction
-                                        [start y direction width]
-                                        [x start direction width]))))
-          ;; Does not have a taken tile in right/bottom direction
-          :when (or (not (< end 11))
-                    (not (tile-taken? board
-                                      (if-right direction
-                                        [end y direction width]
-                                        [x end direction width]))))
-          :let [tile (if-right direction
-                       [start y direction width]
-                       [x start direction width])]]
-      tile)))
+  [board-letters]
+  (for [x (range 0 11)
+        y (range 0 11)
+        [direction current next] [[:right
+                                   (fn [[x y]] x)
+                                   (fn [[x y] i] [(+ i x) y])]
+                                  [:bottom
+                                   (fn [[x y]] y)
+                                   (fn [[x y] i] [x (+ i y)])]]
+        w (range 2 (- (inc 11)
+                      (current [x y])))
+
+        :let [tiles (tiles-taken [x y direction w])
+              blanks (into #{} (filter
+                                 (fn [[a b]]
+                                   (let [letter (nth board-letters (+ a (* b 11)))]
+                                     (nil? letter)))
+                                 tiles))
+              first-tile (first tiles)
+              last-tile (last tiles)
+              prev-tile (next first-tile -1)
+              next-tile (next last-tile 1)]
+
+        :when (not-empty blanks)
+
+        :when (or (not (> (current first-tile) 0))
+                  (nil? (nth board-letters
+                             (+ (nth prev-tile 0)
+                                (* 11 (nth prev-tile 1))))))
+
+        :when (or (not (< (current last-tile) 10))
+                  (nil? (nth board-letters
+                             (+ (nth next-tile 0) 
+                                (* 11 (nth next-tile 1))))))
+        :when (some
+                (fn [[a b]]
+                  (or
+                    (and (< a 10)
+                         (some? (nth board-letters (+ (inc a) (* b 11)))))
+                    (and (< b 10)
+                         (some? (nth board-letters (+ a (* (inc b) 11)))))
+                    (and (> a 0)
+                         (some? (nth board-letters (+ (dec a) (* b 11)))))
+                    (and (> b 0)
+                         (some? (nth board-letters (+ a (* (dec b) 11)))))))
+                blanks)]
+    [x y direction w]))
 
 (defn build-tiles-letter-mapping
   [board]
@@ -135,8 +160,8 @@
 
 (defn rotate-board
   [board]
-  (map (fn [x y direction word]
-         [y x (opposite-direction direction word)]) board))
+  (map (fn [[x y direction word]]
+         [y x (opposite-direction direction) word]) board))
 
 (defn get-extended-board-words
   [board direction]
@@ -151,70 +176,69 @@
   [board-letters available-letters-freq positions]
   (map (fn [[x y direction width :as wordspec]]
          [x y direction width
-          (filter
-            (fn [[word target-word-freq]]
-              (and
-                ;; Is strictly equal to the slot width
-                (= (count word) width)
+          (map first
+               (filter
+                 (fn [[word target-word-freq]]
+                   (and
+                     ;; Is strictly equal to the slot width
+                     (= (count word) width)
 
-                (let [n-non-blanks (count (filter some?
-                                                  (read-letters-from-board board-letters wordspec)))]
-                  ;; Does contain (but not only) blank slots
-                  (and
-                    (not (= width n-non-blanks))
-                    (not (= 0 n-non-blanks))))
+                     ;; Does contain all the non blank slots
+                     (every?
+                       (fn [[[a b] letter]]
+                         (let [board-letter (nth board-letters
+                                                 (+ a (* 11 b)))]
+                           (and
+                             (or (nil? board-letter)
+                                 (= letter board-letter)))))
+                       (map vector (tiles-taken wordspec) word))
 
-                ;; Does contain all the non blank slots
-                (every?
-                  (fn [[[a b] letter]]
-                    (let [board-letter (nth board-letters
-                                            (+ a (* 11 b)))]
-                      (and
-                        (or (nil? board-letter)
-                            (= letter board-letter)))))
-                  (map vector (tiles-taken wordspec) word))
+                     ;; No letters are missing
+                     (let* [njokers (:? available-letters-freq 0)
+                            total-available-letters-freq
+                            (sum-freq-lists (dissoc available-letters-freq :?)
+                                            (read-freq-from-board board-letters wordspec))
+                            freq-diff (sum-freq-lists target-word-freq
+                                                      (scale-freq total-available-letters-freq -1))
+                            nmissing (get-positive-freq-sum freq-diff)] 
+                       (<= nmissing njokers))))
 
-                ;; No letters are missing
-                (let* [njokers (:? available-letters-freq 0)
-                       total-available-letters-freq
-                       (sum-freq-lists (dissoc available-letters-freq :?)
-                                       (read-freq-from-board board-letters wordspec))
-                       freq-diff (sum-freq-lists target-word-freq
-                                                 (scale-freq total-available-letters-freq -1))
-                       nmissing (get-positive-freq-sum freq-diff)] 
-                  (<= nmissing njokers))))
-
-            words-freq-list)])
+                 words-freq-list))])
        positions))
+
+(defn denormalize
+  [words]
+  (->> words
+       (filter (comp not-empty #(nth % 4)))
+       (mapcat (fn [[x y direction width ws]]
+                 (map (fn [w] [x y direction w]) ws)))))
+
+(defn find-crosses
+  [wordspec opposite-direction-words]
+  (let [tiles (into #{} (tiles-taken wordspec))]
+    (filter
+      (fn [[x y direction word]]
+        (not-empty
+          (clojure.set/intersection
+            tiles
+            (into #{} (tiles-taken [x y (opposite-direction direction) word])))))
+      opposite-direction-words)))
+
+(defn cross-word-sets
+  [h-words v-words]
+  [(map (fn [wordspec]
+          {:wordspec wordspec :crosses (find-crosses wordspec v-words)})
+        h-words)])
 
 (defn find-solutions
   [board letters]
   (let* [board-letters (build-tiles-letter-mapping board)
          available-letters-freq (get-letter-frequency letters)
-         horiz-positions (mapcat (fn [[x y]] (find-positions board [x y :right]))
-                                 (get-extended-board-words board :right))
-         vert-positions (mapcat (fn [[x y]] (find-positions board [x y :bottom]))
-                                (get-extended-board-words board :bottom))
+         words (->> (find-positions board-letters)
+                    (find-all-possible-words
+                      board-letters
+                      available-letters-freq)
+                    (denormalize))]
 
-         horiz-words
-         (filter (fn [[x y direction width words]] (not-empty words))
-                 (find-all-possible-words
-                   board-letters
-                   available-letters-freq
-                   horiz-positions))
+    words))
 
-         vert-words
-         (filter (fn [[x y direction width words]] (not-empty words))
-                 (find-all-possible-words
-                   board-letters
-                   available-letters-freq
-                   vert-positions))]
-
-    (doseq [h (mapcat (fn [[x y dir width words]]
-                        (map (fn [[a b]] a) words))
-                      horiz-words)]
-      (pprint ['H h]))
-    (doseq [v (mapcat (fn [[x y dir width words]]
-                        (map (fn [[a b]] a) words))
-                      vert-words)]
-      (pprint ['V v]))))
